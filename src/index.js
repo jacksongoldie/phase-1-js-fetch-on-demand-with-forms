6b8981fb8b17ff6ce909eab98d25de59f418fb91
Goldie <goldiejackson25@yahoo.com>
2021-09-09 17:41:41 -0500

    commit tree 1c52c365990aa3500cb9914c187b0a7bf0650491

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    //two examples of pulling the value from the json object
+    //console.log(e.target.children[1].value);
+       const input = console.log(document.getElementById("searchByID").value);
+   
+    //set up a fetch request for our movies object from the mock JSON
+        fetch('http://localhost:3000/movies/' + input)
+        .then(resp => resp.json())
+        .then(data => console.log(data));
+
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
